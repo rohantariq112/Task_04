34eba91fa5d33a29d93b9cf0bad2b9a7a81a879b
rohantariq112 <rohantariq112@gmail.com>
2022-03-06 13:55:53 +0500

    init

---
+++ Task_04.js
@@ -0,0 +1,38 @@
+
+let ip_addr = prompt("Enter an IP");
+document.write(verifyIP(ip_addr));
+
+
+function verifyIP (IPvalue) {
+errorString = "";
+theName = "IPaddress";
+
+var ipPattern = /^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/;
+var ipArray = IPvalue.match(ipPattern);
+
+if (IPvalue == "0.0.0.0")
+errorString = errorString + theName + ': '+IPvalue+' is a special IP address and cannot be used here.';
+else if (IPvalue == "255.255.255.255")
+errorString = errorString + theName + ': '+IPvalue+' is a special IP address and cannot be used here.';
+if (ipArray == null)
+errorString = errorString + theName + ': '+IPvalue+' is not a valid IP address.';
+else {
+for (i = 0; i < 4; i++) {
+thisSegment = ipArray[i];
+if (thisSegment > 255) {
+errorString = errorString + theName + ': '+IPvalue+' is not a valid IP address.';
+i = 4;
+}
+if ((i == 0) && (thisSegment > 255)) {
+errorString = errorString + theName + ': '+IPvalue+' is a special IP address and cannot be used here.';
+i = 4;
+      }
+   }
+}
+extensionLength = 3;
+if (errorString == "")
+alert ("That is a valid IP address.");
+else
+alert (errorString);
+}
+
